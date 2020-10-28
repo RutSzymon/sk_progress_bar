1c45965bab136e124bb0edbd6e8b1b57a9284c22
Szymon Rut <szymon.rut@unitedideas.pl>
2020-10-28 08:17:10 +0100

    Fix progress_status method in SkProgressBar module

--- lib/sk_progress_bar.rb
+++ lib/sk_progress_bar.rb
@@ -1,12 +1,6 @@
 require "sk_progress_bar/version"
 
 module SkProgressBar
-
-  def self.division_by_cycle(object_count)
-    return unless object_count.present? && object_count.positive?
-    (100 / object_count)
-  end
-
   # Update Progress Bar
   def self.update_progress_bar(percentage, message, jid=nil)
     ActionCable.server.broadcast 'sk_progress_bar_channel',
@@ -17,7 +11,7 @@ module SkProgressBar
 
   # Return Percentage and Message result of cycle
   def self.progress_status(cycle_index, objects_count)
-    percentage = (cycle_index + 1) * division_by_cycle(objects_count)
+    percentage = (cycle_index + 1) * 100 / objects_count
     message = percentage == 100 ? 'Done' : 'Almost done'
     { percentage: percentage, message: message }
   end
